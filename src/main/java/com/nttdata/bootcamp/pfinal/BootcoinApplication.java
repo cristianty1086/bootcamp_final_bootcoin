af7fe06832c43bb75f6e98c1c6f475b7fad91629
crisdbgt1086 <ctinocoy123@gmail.com>
2022-11-07 00:38:05 -0500

    fisrt commit

--- src/main/java/com/nttdata/bootcamp/pfinal/BootcoinApplication.java
+++ src/main/java/com/nttdata/bootcamp/pfinal/BootcoinApplication.java
@@ -1,13 +1,41 @@
 package com.nttdata.bootcamp.pfinal;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
+@Slf4j
+@RequiredArgsConstructor
+@OpenAPIDefinition(info = @Info(title = "App CootCoin", version = "1.0", description = "Documentation APIs v1.0"))
 public class BootcoinApplication {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) {0
 		SpringApplication.run(BootcoinApplication.class, args);
+	}
+
+	@Bean
+	public ReactiveRedisTemplate<String, Parameter> reactiveJsonPostRedisTemplate(
+			ReactiveRedisConnectionFactory connectionFactory) {
+
+		RedisSerializationContext<String, Parameter> serializationContext = RedisSerializationContext
+				.<String, Parameter> newSerializationContext(new StringRedisSerializer())
+				.hashKey(new StringRedisSerializer())
+				.hashValue(new Jackson2JsonRedisSerializer<>(Parameter.class))
+				.build();
+
+		return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
 	}
 
 }
